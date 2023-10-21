238bf98f8b883f6cfbf0688f1dbd1c49d556b646
Adithyan-m <87686715+Adithyan-m@users.noreply.github.com>
2023-10-21 23:56:07 +0530

    Update VCU.ino
    Adding Comments

--- VCU.ino
+++ VCU.ino
@@ -2,25 +2,29 @@
 #include <SPI.h>
 #include <mcp2515.h>
 
+// Define analog input pins
 #define APPS1 A0
 #define APPS2 A1
 #define brake A3
+#define MotorCurrent1 A10
+#define MotorCurrent2 A8
+
+// Define digital input pins
 #define RTDS 10
 #define P1 13
 #define P2 12
 #define SDC_IN 9
 #define START_BTN 8
 #define Brake_Out 7
-#define SC_Relay 4 // relay out
+#define SC_Relay 4 // Relay control
 #define AIR1 22
 #define AIR2 23
 #define MG1 24
 #define MG2 25
-#define MotorCurrent1 A10
-#define MotorCurrent2 A8
 
-#define ADDR1 0x60 // motorout1 DAC address
-#define ADDR2 0x61 // motorout2 DAC address
+// Define DAC (Digital to Analog Converter) addresses
+#define ADDR1 0x60 // Motor output 1 DAC address
+#define ADDR2 0x61 // Motor output 2 DAC address
 
 int RTDS_TRUE = 0;
 unsigned long t = 0;
@@ -35,6 +39,7 @@ float HV_SOC;
 struct can_frame canMsg;
 struct can_frame IMDselfTestMsg;
 
+// Define CAN message IDs
 #define IMD_info_ADDR 0x37
 #define LVTempADDR1 0x01
 #define LVTempADDR2 0x02
@@ -47,9 +52,6 @@ struct can_frame IMDselfTestMsg;
 
 #define Sensors_ADDR 0x10 // CAN
 
-// #define AIR1 1
-// #define AIR2 2
-
 int val1 = 0;
 int val2 = 0;
 int var1 = 0;
@@ -60,26 +62,29 @@ int motorout = 0;
 
 MCP2515 mcp2515(53);
 
+// Function to set DAC output for Motor 1
 void DAC1(int output)
 {
   Serial.print("POWER : ");
   Serial.print("output");
   Wire.beginTransmission(ADDR1);
-  Wire.write(64);                 // cmd to update the DAC
-  Wire.write(output >> 4);        // the 8 most significant bits…
-  Wire.write((output & 15) << 4); // the 4 least significant bits…
+  Wire.write(64);                 // Command to update the DAC
+  Wire.write(output >> 4);        // The 8 most significant bits…
+  Wire.write((output & 15) << 4); // The 4 least significant bits…
   Wire.endTransmission();
 }
 
+// Function to set DAC output for Motor 2
 void DAC2(int output)
 {
   Wire.beginTransmission(ADDR2);
-  Wire.write(64);                 // cmd to update the DAC
-  Wire.write(output >> 4);        // the 8 most significant bits…
-  Wire.write((output & 15) << 4); // the 4 least significant bits…
+  Wire.write(64);                 // Command to update the DAC
+  Wire.write(output >> 4);        // The 8 most significant bits…
+  Wire.write((output & 15) << 4); // The 4 least significant bits…
   Wire.endTransmission();
 }
 
+// Function to map and adjust the throttle input
 int tmap(int inp)
 {
   int tmapout;
@@ -100,35 +105,32 @@ int tmap(int inp)
   }
   return tmapout;
 }
+
+// Function to control vehicle operations
 void control()
 {
   val1 = analogRead(APPS1);
   val2 = analogRead(APPS2);
   brakein = analogRead(brake);
-  // Serial.print(val2);
 
-  //
+  // Handle brake condition
   if (brakein > 205)
   {
     DAC1(0);
     DAC2(0);
     digitalWrite(Brake_Out, HIGH);
-
     Serial.print("brake:");
     Serial.println(brake);
   }
-
-  // sensor failure (outside acceptable range)
+  // Handle sensor failure (outside acceptable range)
   else if (val1 < 103 || val1 > 920 || val2 < 103 || val2 > 920)
   {
     // Serial.println("sensor failure (outside acceptable range)");
     DAC1(0);
     DAC2(0);
   }
-
   else if (abs(val1 - val2) > 103)
   {
-
     t = millis();
     while ((unsigned long)(millis() - t) < 100)
     {
@@ -139,19 +141,16 @@ void control()
         break;
       }
     }
-
     if (abs(var1 - var2) > 103)
     {
       RTDS_TRUE = 0;
       Serial.println("7:APPS implausibility failure");
       DAC1(0);
       DAC2(0);
-
       // Serial.print("APPS implausibility failure :");
       // Serial.println(var1-var2);
     }
   }
-
   else
   {
     int motorpre;
@@ -166,6 +165,7 @@ void control()
   }
 }
 
+// Function to check the readiness to drive system (RTDS)
 void RTDS_check()
 {
   if (digitalRead(START_BTN) == HIGH)
@@ -174,16 +174,17 @@ void RTDS_check()
     if (digitalRead(AIR1) == HIGH && digitalRead(AIR2) == HIGH && digitalRead(SDC_IN) == HIGH && analogRead(brake) > 512 &&
         digitalRead(MG1) == HIGH && digitalRead(MG2) == HIGH)
     {
-      mcp2515.sendMessage(&IMDselfTestMsg); // IMD self test
+      mcp2515.sendMessage(&IMDselfTestMsg); // IMD self-test
       digitalWrite(RTDS, HIGH);
       RTDS_TRUE = 1;
     }
   }
 }
 
+// Function to handle CAN communication
 void CAN()
 {
-  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) // polling for incoming signals
+  if (mcp2515.readMessage(&canMsg) == MCP2515::ERROR_OK) // Polling for incoming signals
   {
     LV_BMS_CAN();
     Sensors_CAN();
@@ -191,12 +192,13 @@ void CAN()
   }
 }
 
+// Function to handle CAN messages related to LV BMS (Battery Management System)
 void LV_BMS_CAN()
 {
   if (canMsg.can_id == LVTempADDR1)
   {
     Serial.print("f1:");
-    for (int i = 0; i < canMsg.can_dlc; i++) // print the LV pack temp2 data
+    for (int i = 0; i < canMsg.can_dlc; i++) // Print the LV pack temperature data
     {
       Serial.print(" ");
       Serial.print(canMsg.data[i]);
@@ -206,7 +208,7 @@ void LV_BMS_CAN()
   if (canMsg.can_id == LVTempADDR2)
   {
     Serial.print("f2:");
-    for (int i = 0; i < canMsg.can_dlc; i++) // print the LV pack temp2 data
+    for (int i = 0; i < canMsg.can_dlc; i++) // Print the LV pack temperature data
     {
       Serial.print(" ");
       Serial.print(canMsg.data[i]);
@@ -217,7 +219,7 @@ void LV_BMS_CAN()
   if (canMsg.can_id == LVVoltADDR)
   {
     Serial.print("c:");
-    for (int i = 0; i < canMsg.can_dlc; i++) // print the LV voltage then current data
+    for (int i = 0; i < canMsg.can_dlc; i++) // Print the LV voltage and current data
     {
       Serial.print(" ");
       Serial.print(canMsg.data[i]);
@@ -226,21 +228,22 @@ void LV_BMS_CAN()
   }
 }
 
+// Function to handle CAN messages related to vehicle sensors
 void Sensors_CAN()
 {
   if (canMsg.can_id == Sensors_ADDR)
   {
-    Serial.println("3:"); // speed
+    Serial.println("3:"); // Speed
     Serial.println(canMsg.data[0]);
 
-    Serial.print("a:"); // wheel speeds
+    Serial.print("a:"); // Wheel speeds
     for (int i = 1; i < 5; i++)
     {
       Serial.print(canMsg.data[i]);
     }
     Serial.println();
 
-    Serial.print("b:"); // accelaration x y z
+    Serial.print("b:"); // Acceleration in x, y, z
     for (int i = 5; i < 8; i++)
     {
       Serial.print(" ");
@@ -255,12 +258,13 @@ void Sensors_CAN()
       Serial.print(canMsg.data[i]);
     }
     Serial.println();
-    Serial.print("3:"); // Eror code motor controller
+    Serial.print("3:"); // Error code from motor controller
     Serial.print(" ");
     Serial.println(canMsg.data[10]);
   }
 }
 
+// Function to handle IMD (Insulation Monitoring Device) CAN messages
 void IMD_CAN()
 {
   if (canMsg.can_id == IMD_info_ADDR)
@@ -269,8 +273,8 @@ void IMD_CAN()
     {
       Serial.println("7:IMD Failure");
     }
-    Serial.print("i:");                      // sending message
-    for (int i = 0; i < canMsg.can_dlc; i++) // print the data
+    Serial.print("i:"); // Sending message
+    for (int i = 0; i < canMsg.can_dlc; i++) // Print the data
     {
       Serial.print(" ");
       Serial.print(canMsg.data[i]);
@@ -278,13 +282,13 @@ void IMD_CAN()
     Serial.println();
   }
 }
+
 void BMS_CAN()
 {
-
   if (canMsg.can_id == Cell_Broadcast_ADDR)
   {
     Serial.print("d:");
-    for (int i = 0; i < canMsg.can_dlc; i++) // print Cell broadcast
+    for (int i = 0; i < canMsg.can_dlc; i++) // Print Cell broadcast
     {
       Serial.print(canMsg.data[i]);
       Serial.print(" ");
@@ -347,14 +351,15 @@ void BMS_CAN()
 
 void setup()
 {
-  Serial.begin(9600); // Serial communication Intialisation
-  Wire.begin();       // I2C Intialisation
+  Serial.begin(9600); // Initialize Serial communication
+  Wire.begin();       // Initialize I2C
 
-  // CAN module  Intialisation
+  // Initialize CAN module
   mcp2515.reset();
   mcp2515.setBitrate(CAN_500KBPS, MCP_8MHZ);
   mcp2515.setNormalMode();
 
+  // Configure IMD self-test message
   IMDselfTestMsg.can_id = 0x22;
   IMDselfTestMsg.can_dlc = 5;
   IMDselfTestMsg.data[0] = 0x21;
@@ -363,24 +368,20 @@ void setup()
   IMDselfTestMsg.data[3] = 0;
   IMDselfTestMsg.data[4] = 0;
 
-  // throttle of both motors to 0
+  // Set throttle of both motors to 0 and activate relay
   DAC1(0);
   DAC2(0);
   delay(10);
   digitalWrite(SC_Relay, HIGH);
 
-  // Timer Intialistion
-  TCCR1A = 0; // set entire TCCR1A register to 0
-  TCCR1B = 0; // same for TCCR1B
-  TCNT1 = 0;  // initialize counter value to 0
-  // set compare match register for 1hz increments
+  // Initialize and configure timer
+  TCCR1A = 0; // Set entire TCCR1A register to 0
+  TCCR1B = 0; // Set entire TCCR1B register to 0
+  TCNT1 = 0;  // Initialize counter value to 0
   OCR1A = 12500; // 200ms
-  // turn on CTC mode
-  TCCR1B |= (1 << WGM12);
-  // Set CS12  for 256  prescaler
-  TCCR1B |= (1 << CS12);
-  // enable timer compare interrupt
-  TIMSK1 |= (1 << OCIE1A);
+  TCCR1B |= (1 << WGM12); // Turn on CTC mode
+  TCCR1B |= (1 << CS12); // Set CS12 for 256 prescaler
+  TIMSK1 |= (1 << OCIE1A); // Enable timer compare interrupt
 
   pinMode(APPS1, INPUT);
   pinMode(APPS2, INPUT);
@@ -391,7 +392,7 @@ void setup()
   pinMode(SDC_IN, INPUT);
   pinMode(START_BTN, INPUT);
   pinMode(Brake_Out, OUTPUT);
-  pinMode(SC_Relay, OUTPUT); // relay out
+  pinMode(SC_Relay, OUTPUT); // Relay control
   pinMode(AIR1, INPUT);
   pinMode(AIR2, INPUT);
   pinMode(MG1, INPUT);
@@ -414,11 +415,10 @@ void loop()
       CAN();
     }
   }
-
   else
   {
     RTDS_check();
-    Serial.println("checking rtds");
+    Serial.println("checking RTDS");
     t = millis();
     while ((unsigned long)(millis() - t) < 50)
     {
@@ -427,12 +427,14 @@ void loop()
   }
 }
 
+// Interrupt Service Routine for timer compare
 ISR(TIMER1_COMPA_vect)
 {
   digitalWrite(SC_Relay, LOW);
   Serial.println("7:VCU Failure");
 }
 
+// Function to calculate checksum for CAN messages
 bool Checksum()
 {
   sum = canMsg.can_id + canMsg.can_dlc;
@@ -440,8 +442,6 @@ bool Checksum()
   {
     sum = sum + canMsg.data[i];
   }
-
   sum = sum >> 8;
-
   return sum == canMsg.data[canMsg.can_dlc - 1];
 }
